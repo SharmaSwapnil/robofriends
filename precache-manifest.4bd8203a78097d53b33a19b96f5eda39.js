5c340bb15cb66648d3a47c823554ea7d1263c16c
SharmaSwapnil <sharma.swapnil1981@gmail.com>
2020-09-05 23:24:18 +0530

    Updates

---
+++ precache-manifest.4bd8203a78097d53b33a19b96f5eda39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e53e2676449f9aedc040934827c416",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5d02c3c1d8d728e87ff7",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5165224c75ad612f70cd",
+    "url": "/robofriends/static/css/main.48a049a7.chunk.css"
+  },
+  {
+    "revision": "5d02c3c1d8d728e87ff7",
+    "url": "/robofriends/static/js/2.aa02b41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.aa02b41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5165224c75ad612f70cd",
+    "url": "/robofriends/static/js/main.b55c6b16.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
